52deb51a0efb9786ffc0441e30a7f046d1af01f2
stoupwhiff <ermesfrancesco.marino@gmail.com>
2022-04-08 17:02:34 +0200

    esercizio 1

---
+++ src/app/components/order-container/order-container.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-order-container',
+  templateUrl: './order-container.component.html',
+  styleUrls: ['./order-container.component.css']
+})
+export class OrderContainerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/components/order-container/order-list/order-single/order-single.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-order-single',
+  templateUrl: './order-single.component.html',
+  styleUrls: ['./order-single.component.css']
+})
+export class OrderSingleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
