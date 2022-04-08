8cdf0afd36097bbcd61bf2defa5d481d6a9b0344
stoupwhiff <ermesfrancesco.marino@gmail.com>
2022-04-08 17:02:03 +0200

    feat: esercizio 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,27 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { UserContainerComponent } from './components/user-container/user-container.component';
+import { OrderContainerComponent } from './components/order-container/order-container.component';
+import { UserListComponent } from './components/user-container/user-list/user-list.component';
+import { UserDetailComponent } from './components/user-container/user-detail/user-detail.component';
+import { UserSingleComponent } from './components/user-container/user-list/user-single/user-single.component';
+import { OrderListComponent } from './components/order-container/order-list/order-list.component';
+import { OrderSingleComponent } from './components/order-container/order-list/order-single/order-single.component';
+import { OrderDetailComponent } from './components/order-container/order-list/order-single/order-detail/order-detail.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserContainerComponent,
+    OrderContainerComponent,
+    UserListComponent,
+    UserDetailComponent,
+    UserSingleComponent,
+    OrderListComponent,
+    OrderSingleComponent,
+    OrderDetailComponent,
   ],
   imports: [
     BrowserModule
