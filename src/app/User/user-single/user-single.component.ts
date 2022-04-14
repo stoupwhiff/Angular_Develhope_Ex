90663d26c969946232c511e44cec27c94ad19833
stoupwhiff <ermesfrancesco.marino@gmail.com>
2022-04-14 17:12:17 +0200

    feat: esercizio 4

---
+++ src/app/User/user-single/user-single.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { User } from 'src/app/models/User';
+
+@Component({
+  selector: 'app-user-single',
+  templateUrl: './user-single.component.html',
+  styleUrls: ['./user-single.component.css']
+})
+export class UserSingleComponent implements OnInit {
+
+  @Input() user!: User
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { UserListComponent } from './User/user-list/user-list.component';
+import { UserSingleComponent } from './User/user-single/user-single.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     UserListComponent,
+    UserSingleComponent,
   ],
   imports: [
     BrowserModule
